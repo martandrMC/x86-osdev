0e358f303040c8df5e31a821964668c5630bcb5b
Martin Andronikos <martin.andronikos@gmail.com>
2026-09-01 03:40:34 +0300

    Better Logs

--- source/loader/main.c
+++ source/loader/main.c
@@ -18,6 +18,7 @@ void disable_cursor(void) {
 	port_out8(0x3D5, curr | (1 << 5));
 }
 
+#define TAB_SIZE 4
 static uint16_t *vga = (uint16_t *) 0xB8000;
 void print_to_vga(const char *str) {
 	static uint16_t cur_x = 0, cur_y = 0;
@@ -25,8 +26,8 @@ void print_to_vga(const char *str) {
 		char c = str[i];
 		switch(c) {
 			case '\0': return;
-			case '\r': cur_x = 0; continue;
-			case '\n': cur_y++; continue;
+			case '\t': cur_x += TAB_SIZE - cur_x % TAB_SIZE; continue;
+			case '\n': cur_x = 0, cur_y++; continue;
 			default: break;
 		}
 		uint32_t offset = cur_y * 80 + cur_x;
@@ -70,22 +71,31 @@ void keyboard_irq(irq_state_t *state) {
 void loader_main(bios_data_t *collected_data) {
 	disable_cursor();
 	for(int i = 0; i < 80 * 25; i++) vga[i] = 0x1F00;
-
 	char buffer[80];
+
+	extern char _loader_base[], _real_end[];
+	extern char _prot_bss_end[], _prot_base[];
+	print_to_vga("Section Bounds:\n");
+	snprintf(buffer, 80, "\tReal: %p - %08tx (%p)\n\tProt: %p - %p (%08tx)\n\n",
+		_loader_base, _loader_base + (uintptr_t) _real_end, _real_end,
+		_prot_base, _prot_bss_end, (ptrdiff_t)(_prot_bss_end - _prot_base));
+	print_to_vga(buffer);
+
+	print_to_vga("BIOS E820 Table:\n");
 	for(int i = 0; i < collected_data->map_entry_count; i++) {
 		uint32_t type = collected_data->map_entries[i].type;
 		if(type > 5) type = 0;
 		snprintf(buffer, 80,
-			"Base: %08X - Size: %08X - %s\r\n",
+			"\tBase: %08X - Size: %08X - %s\n",
 			collected_data->map_entries[i].base,
 			collected_data->map_entries[i].size,
 			map_type_names[type]);
 		print_to_vga(buffer);
 	}
-	print_to_vga("\r\n");
+	print_to_vga("\n");
 
 	snprintf(buffer, 80,
-		"%04X %04X %02X %02X\r\n%04X %02X %02X %04X %04X\r\n",
+		"BPB: %04X %04X %02X %02X %04X %02X %02X %04X %04X\n\n",
 		collected_data->bpb_data->total_sects,
 		collected_data->bpb_data->sects_per_cyl,
 		collected_data->bpb_data->head_count,
